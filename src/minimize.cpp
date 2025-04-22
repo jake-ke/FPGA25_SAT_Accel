3005c2e2e210831d022f2c57e8675dfde974e3c4
milo168 <milo168@g.ucla.edu>
2025-04-22 01:23:53 -0700

    remove debug prints

--- src/minimize.cpp
+++ src/minimize.cpp
@@ -12,7 +12,7 @@ void minimize_dispatch(hls::stream<lit>& toMinimizeStream,
         ap_uint<1> select = 0;
         unsigned int idx = 0;
 
-        /*lit getLit = toMinimizeStream.read();
+        lit getLit = toMinimizeStream.read();
         DISPATCH: while(true){
 	        #pragma HLS pipeline II=1
             #pragma HLS loop_tripcount min=32 max=32
@@ -26,18 +26,6 @@ void minimize_dispatch(hls::stream<lit>& toMinimizeStream,
             #ifdef FPGA_HW
             select++;
             #endif
-        }*/
-
-        
-        DISPATCH: while(true){
-	        #pragma HLS pipeline off
-            #pragma HLS loop_tripcount min=32 max=32
-
-            lit getLit = toMinimizeStream.read();
-            splitMinimizeStream[0].write(getLit);
-            if(toMinimizeStream.empty()){
-                break;
-            }
         }
     }
     splitMinimizeStream[0].write(0);
--- src/solver.cpp
+++ src/solver.cpp
@@ -404,7 +404,7 @@ void solver(clsStatePCIE* clsStates, ap_int<512>* litStore, lit* answerStack,
                 NUM_LITERALS, MAX_LITERAL_ELEMENTS, LITERAL_PAGE_SIZE,
                 learnedStats, litStoreAccessStats, longestClause, error,
                 clauseStoreInputStream1, clauseStoreInputStream2, clauseStoreOutputStream1, clauseStoreOutputStream2, 
-                pqHandlerInput, pqHandlerValue, timerValueStream, conditionStream, cycleCounter, messageStream);
+                pqHandlerInput, pqHandlerValue, timerValueStream, conditionStream, cycleCounter);
             
 
             if(error < 0){
